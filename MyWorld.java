3bb8f8b00e807ce97d066a718994b0ae86a93aa5
AbithanP <abithan.mayo@gmail.com>
2025-04-22 13:54:18 -0400

    Remove Spaces

--- MyWorld.java
+++ MyWorld.java
@@ -1,14 +1,22 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
-    
+
     public MyWorld() {
         super(600, 400, 1);
-        
+
         Hero hero = new Hero();
         addObject(hero, 100, 100);
-        
+
         Banana banana = new Banana();
         addObject(banana, 600, 100);
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }
